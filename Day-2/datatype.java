67b8343306b37b1597da6658d0213f03fcc6f5ab
Akshay Mehta <nkanal38@gmail.com>
2025-08-12 09:50:01 +0530

    add the datatype file

---
+++ Day-2/datatype.java
@@ -0,0 +1,13 @@
+public class datatype {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 20.5;
+        char c = 'A';
+        String d = "Hello, Java!";
+        
+        System.out.println("Integer: " + a);
+        System.out.println("Double: " + b);
+        System.out.println("Character: " + c);
+        System.out.println("String: " + d);
+    }
+}
---
+++ Day-2/userinput.java
@@ -0,0 +1,4 @@
+
+public class userinput {
+    public static void main(String[] args) {
+        String name = System.console().readLin
