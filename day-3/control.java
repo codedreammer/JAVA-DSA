bd6b28a12467acf2f20c6170a2ae8ac4c970b286
Akshay Mehta <nkanal38@gmail.com>
2025-08-25 11:39:09 +0530

    add the file

---
+++ day-3/control.java
@@ -0,0 +1,66 @@
+// public class control {
+//     public static void main(String[] args){
+//             boolean teacher = true;
+//         if (teacher) { 
+//             System.out.println("class started");
+//         } else {
+//             System.out.println("free class");
+//     }
+// }
+// }
+
+// import java.util.Scanner;
+
+// public class control {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+
+//         System.out.print("Enter your name: ");
+//         String name = scanner.nextLine(); 
+
+//         System.out.print("Enter your age: ");
+//         int age = scanner.nextInt(); 
+
+//         System.out.println("Hello, " + name + "! You are " + age + " years old.");
+
+//         scanner.close();
+//     }
+// }
+
+
+
+// public class control {
+//     public static void main(String[] args)
+//     {
+//         int age = 18;
+//         if (age >= 18) {
+//             System.out.println("you are eligible to vote");
+//         } else {
+//             System.out.println("you are not eligible to vote");
+//         }
+//     }
+// }
+
+
+
+// public class control {
+//     public static void main(String[] args)
+//     {
+//         int day = 3;
+// switch (day) {
+//     case 1: System.out.println("Monday"); break;
+//     case 2: System.out.println("Tuesday"); break;
+//     case 3: System.out.println("Wednesday"); break;
+//     default: System.out.println("Invalid day");
+//         }
+//     }
+// }
+
+public class control {
+    public static void main(String[] args)
+    {
+        for (int i = 1; i <= 5; i++) {
+    System.out.println("Count: " + i);
+        }
+    }
+}
