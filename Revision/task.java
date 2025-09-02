913e85ad76c87d1cbf21aa59cdae98098452dfe0
Akshay Mehta <nkanal38@gmail.com>
2025-09-02 08:36:57 +0530

    add the file

---
+++ Revision/task.java
@@ -0,0 +1,85 @@
+package Revision;
+
+public class task {
+    // public static void main(String[] args) {
+    //     System.out.println("Hello World");
+    //     System.out.println("work done" );
+    // }
+
+    // public static void main(String[] args) {
+    //     System.out.println(23);
+    //     System.out.println(2333);
+    //     System.out.println(2222);
+    // }
+
+    // public static void main(String[] args) {
+//         System.out.println(2+3);
+//         System.out.println(7*5);
+//         System.err.println(10/5);
+//         System.out.println("10" + 5);
+// }
+
+    // public static void main(String[] args) {
+    //     String name = "John";
+    //     String position = "CR";
+    //     String check = "when";
+    //     System.err.println(name);
+    //     System.out.println(position);
+    //     System.out.println(check);
+
+
+    // }
+
+    // public static void main(String[] args) {
+    //     int myNum = 23;
+    //     int myNum1 = 45;
+    //     System.out.println(myNum);
+    //     System.out.println(myNum1);
+    // }
+    // public static void main(String[] args) {
+        
+    
+    //     int muNum = 23;
+    //     float myFloatNum = 5.99f;
+    //     char myLetter = 'D';
+    //     boolean myBool = true;
+    //     String myText = "Hello";
+    //     System.out.println(muNum);
+    //     System.out.println(myFloatNum);
+    //     System.out.println(myLetter);
+    //     System.out.println(myBool);
+    //     System.out.println(myText);
+
+
+    // }
+    // public static void main(String[] args) {
+    //     String firstName = "John";
+    //     String lastName = "Doe";
+    //     String fullName = firstName + " " + lastName;
+    //     System.err.println(fullName);
+    // }
+    // public static void main(String[] args) {
+    //     int x = 10;
+    //     int y = 5;
+    //     int z = x + y;
+    //     System.err.println(z);
+    // }
+
+    // public static void main(String[] args) {
+    //     System.err.println("Number of arguments: " + args.length);
+    //     for (int i = 0; i < args.length; i++) {
+    //         System.out.println("argument " + i + ": "+ args[i]);
+    //     }
+        
+    // }
+    // public static void main(String[] args) {
+    //     int x, y, z;
+    //     x = y = z = 50;
+    //     System.out.println(x + y + z);
+    // }
+    public static void main(String[] args) {
+        int x = 5, y = 10;
+        System.out.println(x != y);
+    }
+}
+
