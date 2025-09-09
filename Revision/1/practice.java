de44e62a83a80a6e55779bcca3d74acc6789135e
Akshay Mehta <nkanal38@gmail.com>
2025-09-09 09:24:24 +0530

    add the file

---
+++ Revision/1/practice.java
@@ -0,0 +1,37 @@
+// public class practice {
+// public static void main(String[] args) {
+//     String[] cars = {"apple", "banana", "cherry"};
+//     System.err.println(cars[0]);
+    
+    
+// }
+
+// }
+
+// public class practice {
+//     public static void main(String[] args) {
+//         String[] Phone = {"apple", "MI", "Readme"};
+//         System.err.println(Phone[2]);
+//         }
+//     }
+
+
+// public class practice {
+//     public static void main(String[] args) {
+//         String[] Laptop = {"apple", "MI", "Readme"};
+
+//         System.err.println(Laptop[4]);
+//         }
+//     }
+
+public class practice {
+    public static void main(String[] args) {
+        String[] cars = new String[4];
+        cars[0] = "BMW";
+        cars[1] = "Audi";
+        cars[2] = "Ford";
+
+        System.out.println(cars[0]);
+    }
+
+}
